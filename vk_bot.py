58866d6e0e1270f5711275e62533589570cca4ca
Pavel Rudenko <pogorus@gmail.com>
2021-11-29 22:47:19 +0300

    Refactoring

--- vk_bot.py
+++ vk_bot.py
@@ -1,17 +1,22 @@
 from random import randrange
 from datetime import datetime
+import time
 import requests
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+import vk_db
 
-user_token = 'user token'
-token = 'group token'
+
+user_token = 'USER TOKEN'
+token = 'TOKEN'
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
 
+vk_db.start_db()
+
 class VkUser:
-    def __init__(self, user_id, bdate, sex, city):
+    def __init__(self, user_id, bdate, sex, city, counter = 0):
         self.user_id = user_id
         if len(bdate[0]) == 1:
             bdate[0] = f'0{bdate[0]}'
@@ -20,9 +25,8 @@ class VkUser:
         self.age = (int(datetime.today().strftime('%Y%m%d')) - int(''.join(bdate[::-1]))) // 10000
         self.sex = sex
         self.city = city
-        self.counter = 0
+        self.counter = counter
 
-counter = 0
 
 def get_photo(id):
     response = requests.get('https://api.vk.com/method/photos.get',
@@ -36,11 +40,14 @@ def get_photo(id):
     sorted_photo = sorted(photo_dict.items(), key=lambda item: item[1])
     return sorted_photo[-1:-4:-1]
 
+
 def write_msg(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7)})
 
+
 def send_attachment(user_id, owner_id, photo_id):
     vk.method('messages.send', {'user_id': user_id, 'message': '', 'attachment': f'photo{owner_id}_{photo_id}', 'random_id': randrange(10 ** 7)})
+
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
@@ -49,87 +56,138 @@ for event in longpoll.listen():
             request = event.text
             user_id = event.user_id
 
-            response = requests.get('https://api.vk.com/method/users.get',
-                                    params={'access_token': token, 'v': '5.131', 'user_ids': request,
-                                            'fields': 'city, sex, bdate'}).json()
+            finder_params = {'access_token': token, 'v': '5.131', 'user_ids': request, 'fields': 'city, sex, bdate'}
 
-            vk_id = response['response'][0]['id']
+            response = requests.get('https://api.vk.com/method/users.get', params=finder_params).json()
+
             try:
-                bdate = response['response'][0]['bdate'].split('.')
+                if response['error']:
+                    write_msg(user_id, 'Введите ID пользователя для которого хотите найти пару.')
+                    for event in longpoll.listen():
+                        if event.type == VkEventType.MESSAGE_NEW:
+                            if event.to_me:
+                                request = event.text
+                                user_id = event.user_id
+                                finder_params = {'access_token': token, 'v': '5.131', 'user_ids': request, 'fields': 'city, sex, bdate'}
+                                response = requests.get('https://api.vk.com/method/users.get', params=finder_params).json()
+                                try:
+                                    if response['response']:
+                                        break
+                                except KeyError:
+                                    write_msg(user_id, 'Пользователя с данным ID не существует, проверьте правильность ввода, или введите другой ID.')
             except KeyError:
-                bdate = []
-            try:
-                sex = response['response'][0]['sex']
-            except KeyError:
-                sex = ''
-            try:
-                city = response['response'][0]['city']['id']
-            except KeyError:
-                city = ''
+                pass
 
-            if bdate == [] or len(bdate) < 3:
-                write_msg(user_id, 'Укажите дату рождения в формате: дд.мм.гггг')
-                for event in longpoll.listen():
-                    if event.type == VkEventType.MESSAGE_NEW:
+            finder_id = response['response'][0]['id']
 
-                        if event.to_me:
-                            request = event.text
-                            user_id = event.user_id
-                            bdate = request.split('.')
-                            break
+            finders_list = vk_db.get_finders_list()
 
-            if sex == '':
-                write_msg(user_id, 'Укажите пол: 1 - женский; 2 - мужской')
-                for event in longpoll.listen():
-                    if event.type == VkEventType.MESSAGE_NEW:
+            if str(finder_id) in finders_list:
+                bdate = vk_db.get_bdate(finder_id)
+                sex = vk_db.get_sex(finder_id)
+                city = vk_db.get_city(finder_id)
+                counter = vk_db.get_counter(finder_id)
 
-                        if event.to_me:
-                            request = event.text
-                            user_id = event.user_id
-                            sex = request
-                            break
+            else:
+                try:
+                    bdate = response['response'][0]['bdate'].split('.')
+                except KeyError:
+                    bdate = []
+                try:
+                    sex = response['response'][0]['sex']
+                except KeyError:
+                    sex = ''
+                try:
+                    city = response['response'][0]['city']['id']
+                except KeyError:
+                    city = ''
+                counter = 0
 
-            if city == '':
-                write_msg(user_id, 'Укажите город')
-                for event in longpoll.listen():
-                    if event.type == VkEventType.MESSAGE_NEW:
+                if bdate == [] or len(bdate) < 3:
+                    write_msg(user_id, 'Укажите дату рождения в формате: дд.мм.гггг')
+                    for event in longpoll.listen():
+                        if event.type == VkEventType.MESSAGE_NEW:
+                            if event.to_me:
+                                request = event.text
+                                user_id = event.user_id
+                                bdate = request.split('.')
+                                if bdate == [] or len(bdate) < 3 or not 0 < int(bdate[0]) <= 31 or not 0 < int(bdate[1]) <= 12 or not 1900 < int(bdate[2]) < datetime.today().year:
+                                    write_msg(user_id, 'Дата рождения указанна некорректно')
+                                    write_msg(user_id, 'Укажите дату рождения в формате: дд.мм.гггг')
+                                else:
+                                    break
 
-                        if event.to_me:
-                            request = event.text
-                            user_id = event.user_id
-                            city = request.lower()
-                            city_response = requests.get('https://api.vk.com/method/database.getCities',
-                                    params={'access_token': user_token, 'v': '5.131', 'country_id': 1,
-                                            'q': city, 'need_all': '0', 'count': 1}).json()
-                            city = city_response['response']['items'][0]['id']
-                            break
+                if sex == '':
+                    write_msg(user_id, 'Укажите пол: 1 - женский; 2 - мужской')
+                    for event in longpoll.listen():
+                        if event.type == VkEventType.MESSAGE_NEW:
+                            if event.to_me:
+                                request = event.text
+                                user_id = event.user_id
+                                sex = request
+                                if not 1 <= sex <= 2:
+                                    write_msg(user_id, 'Пол указан некорректно')
+                                    write_msg(user_id, 'Укажите пол: 1 - женский; 2 - мужской')
+                                else:
+                                    break
 
-            user = VkUser(vk_id, bdate, sex, city)
+                if city == '':
+                    write_msg(user_id, 'Укажите город')
+                    for event in longpoll.listen():
+                        if event.type == VkEventType.MESSAGE_NEW:
+                            if event.to_me:
+                                request = event.text
+                                user_id = event.user_id
+                                city = request.lower()
+                                city_response = requests.get('https://api.vk.com/method/database.getCities',
+                                        params={'access_token': user_token, 'v': '5.131', 'country_id': 1,
+                                                'q': city, 'need_all': '0', 'count': 1}).json()
+                                try:
+                                    city = city_response['response']['items'][0]['id']
+                                    break
+                                except IndexError:
+                                    write_msg(user_id, 'Город не найден')
+                                    write_msg(user_id, 'Укажите город')
 
-            search_response = requests.get('https://api.vk.com/method/users.search',
-                                           params={'access_token': user_token, 'v': '5.131', 'sort': 0,
+                if len(bdate[0]) == 1:
+                    bdate[0] = f'0{bdate[0]}'
+                if len(bdate[1]) == 1:
+                    bdate[1] = f'0{bdate[1]}'
+
+                vk_db.add_new_finder(finder_id, bdate, sex, city)
+
+            user = VkUser(finder_id, bdate, sex, city, counter)
+
+            search_params = {'access_token': user_token, 'v': '5.131', 'sort': 0,
                                                    'sex': 1 if user.sex == 2 else 2,
                                                    'count': 1, 'status': 6, 'age_from': user.age,
-                                                   'age_to': user.age, 'offset': counter,
+                                                   'age_to': user.age, 'offset': user.counter,
                                                    'has_photo': 1, 'city': user.city, 'fields': 'id',
-                                                   'is_closed': 0}).json()
-            counter += 1
+                                                   'is_closed': 0}
+            search_response = requests.get('https://api.vk.com/method/users.search', params=search_params).json()
+
+            user.counter += 1
+            vk_db.update_counter(finder_id)
 
             if search_response['response']['items'] == [] or search_response['response']['items'][0]['is_closed']:
                 while search_response['response']['items'] == [] or search_response['response']['items'][0]['is_closed']:
-                    search_response = requests.get('https://api.vk.com/method/users.search',
-                                                   params={'access_token': user_token, 'v': '5.131', 'sort': 0,
-                                                           'sex': 1 if user.sex == 2 else 2,
-                                                           'count': 1, 'status': 6, 'age_from': user.age,
-                                                           'age_to': user.age, 'offset': counter,
-                                                           'has_photo': 1, 'city': user.city, 'fields': 'id',
-                                                           'is_closed': 0}).json()
-                    counter += 1
+                    search_params = {'access_token': user_token, 'v': '5.131', 'sort': 0,
+                                     'sex': 1 if user.sex == 2 else 2,
+                                     'count': 1, 'status': 6, 'age_from': user.age,
+                                     'age_to': user.age, 'offset': user.counter,
+                                     'has_photo': 1, 'city': user.city, 'fields': 'id',
+                                     'is_closed': 0}
+                    search_response = requests.get('https://api.vk.com/method/users.search', params=search_params).json()
+                    user.counter += 1
+                    vk_db.update_counter(finder_id)
+                    time.sleep(0.5)
 
-            write_msg(user_id, f'https://vk.com/id{search_response["response"]["items"][0]["id"]}')
+            found_id = search_response["response"]["items"][0]["id"]
+            write_msg(user_id, f'https://vk.com/id{found_id}')
 
-            photos = get_photo(search_response['response']['items'][0]['id'])
+            photos = get_photo(found_id)
 
             for photo in photos:
-                send_attachment(user_id, search_response['response']['items'][0]['id'], photo[0])
+                send_attachment(user_id, found_id, photo[0])
 
+            vk_db.add_new_found(finder_id, found_id)
---
+++ vk_db.py
@@ -0,0 +1,68 @@
+import sqlalchemy
+
+engine = sqlalchemy.create_engine('postgresql://LOGIN:PASSWORD@localhost:5432/vkinder')
+connection = engine.connect()
+
+def start_db():
+    connection.execute(f'''create table if not exists Finder (
+                                                vk_id varchar(20) primary key, 
+                                                bdate varchar(10) not null, 
+                                                sex integer not null,
+                                                city integer not null,
+                                                counter integer);''')
+
+    connection.execute(f'''create table if not exists Found (
+                                                vk_id varchar(20) primary key);''')
+
+    connection.execute(f'''create table if not exists FinderFound (
+                                                finder_id varchar(20) not null references Finder(vk_id),
+                                                found_id varchar(20) not null references Found(vk_id),
+                                                constraint finderfound_pk primary key (finder_id, found_id));''')
+
+
+def get_finders_list():
+    finders_vk_id = connection.execute('''SELECT vk_id FROM finder;''').fetchall()
+    finders_list = []
+    for finder in finders_vk_id:
+        finders_list.append(finder[0])
+    return finders_list
+
+
+def get_bdate(finder_id):
+    sql_bdate = connection.execute(f'''SELECT bdate FROM finder WHERE vk_id = '{finder_id}';''').fetchone()
+    bdate = sql_bdate[0].split('.')
+    return bdate
+
+
+def get_sex(finder_id):
+    sex = connection.execute(f'''SELECT sex FROM finder WHERE vk_id = '{finder_id}';''').fetchone()
+    sex = sex[0]
+    return sex
+
+
+def get_city(finder_id):
+    city = connection.execute(f'''SELECT city FROM finder WHERE vk_id = '{finder_id}';''').fetchone()
+    city = city[0]
+    return city
+
+
+def get_counter(finder_id):
+    counter = connection.execute(f'''SELECT counter FROM finder WHERE vk_id = '{finder_id}';''').fetchone()
+    counter = counter[0]
+    return counter
+
+
+def add_new_finder(finder_id, bdate, sex, city):
+    connection.execute(
+        f'''INSERT INTO finder(vk_id, bdate, sex, city, counter) values ('{finder_id}', '{".".join(bdate)}', {sex}, {city}, 0);''')
+
+
+def update_counter(finder_id):
+    connection.execute(f'''UPDATE finder SET counter=counter+1 WHERE vk_id = '{finder_id}';''')
+
+
+def add_new_found(finder_id, found_id):
+    connection.execute(
+        f'''INSERT INTO found(vk_id) values ('{found_id}');''')
+    connection.execute(
+        f'''INSERT INTO finderfound(finder_id, found_id) values ('{finder_id}', '{found_id}');''')
